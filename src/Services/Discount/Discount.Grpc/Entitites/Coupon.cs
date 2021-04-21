8d8590bca9a50167c18c0f7c7934bd21fb3a1cf9
Diego Moreno <diegosvm@hotmail.com>
2021-04-21 12:22:51 -0300

    feat: Consuming Discount Grps Service from Basket Microservices When adding a Cart Item

--- src/Services/Discount/Discount.Grpc/Entitites/Coupon.cs
+++ src/Services/Discount/Discount.Grpc/Entitites/Coupon.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Discount.Grpc.Entitites
+﻿namespace Discount.Grpc.Entitites
 {
     public class Coupon
     {
